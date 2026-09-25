50d0c5e80cc344a86c9f54dd3b0479dada82d35d
Keith Henderson <keith.donaldh@gmail.com>
2026-09-25 08:33:55 -0400

    update

--- init.lua
+++ init.lua
@@ -434,3 +434,32 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.softtabstop = 4
 	end,
 })
+
+-- ============================================================
+-- Telescope
+-- ============================================================
+
+local plugin_dir = vim.fn.stdpath("config") .. "/plugins"
+
+vim.opt.rtp:prepend(plugin_dir .. "/plenary.nvim")
+vim.opt.rtp:prepend(plugin_dir .. "/telescope.nvim")
+
+require("telescope").setup({})
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+	desc = "Find files",
+})
+
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+	desc = "Live grep",
+})
+
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+	desc = "Find buffers",
+})
+
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
+	desc = "Help",
+})
